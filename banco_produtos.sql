4f27ada0bec0e388d434d997b20f34a3041ff984
MichaelDCA <michaeldouglascruzalves@gmail.com>
2024-12-05 21:12:46 -0300

    Update banco_produtos.sql

--- banco_produtos.sql
+++ banco_produtos.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 04/12/2024 às 03:02
+-- Tempo de geração: 06/12/2024 às 01:12
 -- Versão do servidor: 10.4.32-MariaDB
 -- Versão do PHP: 8.2.12
 
@@ -35,6 +35,54 @@ CREATE TABLE `produtos` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
+-- Despejando dados para a tabela `produtos`
+--
+
+INSERT INTO `produtos` (`id`, `nome`, `preco`, `quantidade`) VALUES
+(14, 'Arroz Integral', 12.99, 300),
+(15, 'Feijão Preto', 7.49, 150),
+(16, 'Açúcar Refinado', 4.89, 200),
+(17, 'Farinha de Trigo', 5.59, 180),
+(18, 'Óleo de Soja', 6.99, 120),
+(19, 'Macarrão Espaguete', 3.89, 220),
+(20, 'Leite Integral', 4.50, 100),
+(21, 'Café Torrado', 9.99, 80),
+(22, 'Sal Refinado', 2.59, 250),
+(23, 'Biscoito Recheado', 6.00, 500),
+(24, 'Margarina', 3.75, 190),
+(25, 'Suco de Laranja', 5.25, 160),
+(26, 'Maionese', 7.00, 110),
+(27, 'Chocolate ao Leite', 4.50, 140),
+(28, 'Papel Higiênico', 3.00, 300),
+(29, 'Batata Doce', 3.80, 230),
+(30, 'Macarrão para Lasanha', 4.50, 180),
+(31, 'Alho Desidratado', 2.99, 170),
+(32, 'Tomate em Lata', 3.75, 120),
+(33, 'Molho de Tomate', 4.00, 200),
+(34, 'Cebola', 2.20, 250),
+(35, 'Carne Moída', 19.99, 60),
+(36, 'Frango Congelado', 12.00, 150),
+(37, 'Linguiça Calabresa', 9.99, 130),
+(38, 'Picanha', 35.00, 80),
+(39, 'Peito de Frango', 15.00, 200),
+(40, 'Queijo Mussarela', 10.99, 180),
+(41, 'Presunto', 8.00, 160),
+(42, 'Salsicha', 4.90, 220),
+(43, 'Requeijão Cremoso', 6.50, 110),
+(44, 'Maçã', 5.50, 300),
+(45, 'Banana Prata', 4.00, 250),
+(46, 'Laranja', 3.00, 180),
+(47, 'Melancia', 7.00, 100),
+(48, 'Pera', 6.50, 150),
+(49, 'Uva', 8.00, 120),
+(50, 'Morango', 10.00, 80),
+(51, 'Alface', 2.00, 200),
+(52, 'Cenoura', 3.00, 250),
+(53, 'Beterraba', 2.80, 230),
+(54, 'Abóbora', 3.90, 210),
+(55, 'Pepino', 2.50, 300);
+
+--
 -- Índices para tabelas despejadas
 --
 
@@ -52,7 +100,7 @@ ALTER TABLE `produtos`
 -- AUTO_INCREMENT de tabela `produtos`
 --
 ALTER TABLE `produtos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=56;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
